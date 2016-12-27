cfcf97266cf9658197bca7df2dbae1aadadc8876
RryLee <qq215672398@126.com>
2016-12-27 19:18:00 +0800

    MOD 添加github地址

--- render.sh
+++ render.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-aglio -i api.apib -s --theme-template ./templates/index.jade --theme-style default --theme-style ./styles/_variable.less --theme-style ./styles/rewrite.less
+aglio -i api.apib -s --theme-template ./templates/index.jade --theme-style default --theme-style ./styles/rewrite.less
